97abbeb5cf917400c4f316b2c238603663ee1796
unknown <o.t.wathsala@gmail.com>
2019-05-11 23:25:08 +0530

    test two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 
@@ -8,9 +8,11 @@ import { TestComponent } from './test/test.component';
   declarations: [
     AppComponent,
     TestComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
