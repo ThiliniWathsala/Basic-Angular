f12252f76e7fa5b731ea10a44c4360864e328429
unknown <o.t.wathsala@gmail.com>
2019-05-11 21:16:48 +0530

    check  string interpolation

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  name:string="Thilini ";
+
+  showName(){
+    return "hello "+this.name;
+  }
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
